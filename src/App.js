21cbe605bcf5992380cf4522ef20e445b0659e3f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-18 01:13:50 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Navbar from './components/Navbar/Navbar';
 import Blog from './components/Blog/Blog';
 import Services from './components/Services/Services'
 import AboutMe from './components/About-me/AboutMe'
+import Login from './components/Log_In/Login';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
+        <Route path='login' element={<Login/>}></Route>
         <Route path='/' element={<Home/>}></Route>
         <Route path='blog' element={<Blog/>}></Route>
         <Route path='service' element={<Services/>}></Route>
