ef0e4ae34626bdd2d7cf382b637b881101aa465a
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-18 00:34:15 +0600

    react router complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar';
+import Blog from './components/Blog/Blog';
+import Services from './components/Services/Services'
+import AboutMe from './components/About-me/AboutMe'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='blog' element={<Blog/>}></Route>
+        <Route path='service' element={<Services/>}></Route>
+        <Route path='about_me' element={<AboutMe/>}></Route>
+        
+      </Routes>
+       
       
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,24 +6,24 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" href="#">Navbar</Link>
+                <div className="container-fluid container">
+                    <Link className="navbar-brand" to="#">Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" href="#">Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" href="#">Features</Link>
+                                <Link className="nav-link" to="/blog">Blog</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" href="#">Pricing</Link>
+                                <Link className="nav-link" to="/service">Service</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link disabled">Disabled</Link>
+                                <Link className="nav-link " to="/about_me">About Me</Link>
                             </li>
                         </ul>
                     </div>
