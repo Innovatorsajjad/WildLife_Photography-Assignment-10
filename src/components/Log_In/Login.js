0825573378527843a925162f4cb32f7a4213771f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-21 13:26:45 +0600

    three website log in button added

--- src/components/Log_In/Login.js
+++ src/components/Log_In/Login.js
@@ -3,16 +3,6 @@ import "./Login.css"
 
 const Login = () => {
 
-
-
-
-
-
-
-
-
-
-    
     return (
         <div className='container mt-5'>
             <form className=' Full_form w-50 mx-auto mt-5 border p-3 rounded shadow-sm p-3 mb-5 bg-body'>
@@ -24,10 +14,6 @@ const Login = () => {
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
